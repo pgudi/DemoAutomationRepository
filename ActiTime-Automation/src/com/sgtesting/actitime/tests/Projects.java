45fe7a19b13a53f479a1f65c4093768d0541a0b4
pgudi <prabhakargudi@gmail.com>
2022-01-13 09:56:05 +0530

    Actitime Automation Framework Level01

---
+++ ActiTime-Automation/src/com/sgtesting/actitime/tests/Projects.java
@@ -0,0 +1,71 @@
+package com.sgtesting.actitime.tests;
+
+public class Projects extends Initialize{
+	
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void createProject()
+	{
+		try
+		{
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void modifyProject()
+	{
+		try
+		{
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void deleteProject()
+	{
+		try
+		{
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+}
---
+++ ActiTime-Automation/src/com/sgtesting/actitime/tests/Tasks.java
@@ -0,0 +1,71 @@
+package com.sgtesting.actitime.tests;
+
+public class Tasks extends Initialize{
+	
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void createTask()
+	{
+		try
+		{
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void modifyTask()
+	{
+		try
+		{
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void deleteTask()
+	{
+		try
+		{
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+}
